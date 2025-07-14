12b93ad09a5313cbf441863f970d1a5f0841d012
Dephy <107806371+jeffreykang03@users.noreply.github.com>
2025-07-14 16:05:32 -0700

    update db-test.js to work with new columns

--- db-test/db-test.js
+++ db-test/db-test.js
@@ -14,25 +14,41 @@ client.connect()
   .then(() => console.log('PostgreSQL Connected!'))
   .catch((err) => console.error('Connection error', err.stack));
 
-//test inserting
-const insertQuery = `
-  INSERT INTO users (username, password_hash, email)
-  VALUES ('nodejsinserttest', 'innoutcrambuthashed', 'insertsuccessful@ucsd.edu')
-`;
+// //test inserting
+// const insertUserQuery = `
+//     INSERT INTO users (username, password_hash, email, full_name, major, year, bio, created_at, updated_at)
+//     VALUES ('kevinyang123', 'hashedpassword', 'alice@ucsd.edu', 'Kevin Yang', 'Computer Science', 'Senior', 'I love In-N-Out', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
+// `;
 
-client.query(insertQuery)
-  .then(() => console.log('Test user inserted!\n'))
-  .catch((err) => console.error('Error inserting test user\n', err.stack));
+// client.query(insertUserQuery)
+//   .then(() => console.log('Test user inserted!\n'))
+//   .catch((err) => console.error('Error inserting test user\n', err.stack));
+
+// const insertEventQuery = `
+//     INSERT INTO events (title, description, location, date_and_time, creator_id, created_at, event_type, status, capacity, tags, participant_ids)
+//     VALUES ('CS101 Study Group', 'Study group for CS101: Introduction to Computer Science', 'Room 101, UCSD', '2025-09-15 10:00:00', '37bd4d1a-fd0e-4f43-8fd5-d3d436da39e2'::UUID, CURRENT_TIMESTAMP, 'In-person', 'Upcoming', 30, 
+//     ARRAY['CS101', 'Computer Science', 'Quiet'], ARRAY['37bd4d1a-fd0e-4f43-8fd5-d3d436da39e2'::UUID])
+// `;
+
+// client.query(insertEventQuery)
+//   .then(() => console.log('Test user inserted!\n'))
+//   .catch((err) => console.error('Error inserting test Event\n', err.stack));
 
 //test querying
-const selectQuery = 'SELECT * FROM users';
+const selectUsersQuery = 'SELECT * FROM users';
+const selectEventsQuery = 'SELECT * FROM events';
 
-client.query(selectQuery)
+client.query(selectUsersQuery)
   .then(res => {
     console.log('Users:\n', res.rows);
-    client.end();
+    return client.query(selectEventsQuery);
+  })
+  .then(res => {
+    console.log('Events:\n', res.rows);
   })
   .catch(err => {
-    console.error('Error querying users\n', err.stack);
+    console.error('Error querying data\n', err.stack);
+  })
+  .finally(() => {
     client.end();
   });
