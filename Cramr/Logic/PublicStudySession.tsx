797db811479abb3d875edcbc750b776ab40cd7b4
Tyler Vo <tylervo02@yahoo.com>
2025-07-21 13:34:09 -0700

    Installed dependencies for google maps services project wide and started on function to convert from address to coordinates.

--- Cramr/Logic/PublicStudySession.tsx
+++ Cramr/Logic/PublicStudySession.tsx
@@ -1,3 +1,4 @@
+
 class PublicStudySession extends StudySession{
     private attendance: Array<User>;
 
@@ -8,5 +9,17 @@ class PublicStudySession extends StudySession{
 
     // Add logic here, most likely with Google Maps API.
     // It can be viewed by everyone, BUT it can only be held in public spaces.
+
+    // turning address to coordinates so that it can be displayed in frontend (although we might store the location as an array instead for security purposes)
+    //returns a google LatLng object, which works well with front end map
+    public addressToCoordinates(location: string){
+        var geocoder = new google.maps.Geocoder(); //might be inefficient because we're calling Google Maps API for everytime we're converting to coordinates
+        geocoder.geocode( { 'address': location}, function(results, status){
+            if(status == 'OK'){
+                return results[0].geometry.location; 
+            }
+        });
+        return [-1, -1]
+    }
 
 }
