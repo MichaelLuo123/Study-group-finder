cb850341615ec4b8d8073984466a88b36e54d00d
Tyler Vo <tylervo02@yahoo.com>
2025-08-22 17:25:51 -0700

    Have the screen pop out without the animation. Will put in individual animation next commit

--- Cramr/app/_layout.tsx
+++ Cramr/app/_layout.tsx
@@ -31,6 +31,7 @@ export default function RootLayout() {
           <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
             <Stack screenOptions={{
               headerShown: false,
+              animation: 'none'
             }}>
               {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
               <Stack.Screen name="+not-found" /> */}
